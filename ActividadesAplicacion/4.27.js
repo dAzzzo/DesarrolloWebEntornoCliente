6d1a2bfbd1b14aa97663ce7da2b5547bc7d572d7
Daniel <dandcom0601@g.educaand.es>
2023-11-21 14:55:46 +0100

    pepe

--- ActividadesAplicacion/4.27.js
+++ ActividadesAplicacion/4.27.js
@@ -1,15 +1,15 @@
 // Crea una funcion autoejecutable  que muestre el mensaje <Comenzando...> y tres segundos despues escriba <Finalizado...>
 
 function auto() {
-  return new Promise((resolve) => {
-    (async () => {
-      document.write("Comenzando...");
+  (async () => {
+    document.write("Comenzando...");
 
-      // Espera tres segundos
-      await new Promise((innerResolve) => setTimeout(innerResolve, 3000));
-
+    // Espera tres segundos
+    setTimeout(() => {
       document.write("Finalizado...");
-      resolve("Finalizado...");
-    })();
-  });
+    }, 3000);
+  })();
 }
+
+// Llamada a la función auto
+auto();
---
+++ ActividadesAplicacion/4.28.js
@@ -0,0 +1,2 @@
+// Programa una funcion que reciba como parametros dos arrays de 4x4 y devuelva untercer array
+// relleno de ceros excepto en aquellas posiciones en las que los dos primeros arrays tienen valores iguales
