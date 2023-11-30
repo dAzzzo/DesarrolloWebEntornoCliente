4d32ca2b7cc7c9fc0c9f262ea88ad7c9d94e155f
Daniel <dandcom0601@g.educaand.es>
2023-11-30 10:40:15 +0100

    cambio nuevo

--- actividadesLibro/unidad 5/5.1.js
+++ actividadesLibro/unidad 5/5.1.js
@@ -1,11 +1,22 @@
-class TelefonoMobil {
+class Dispositovo {
+  marca = "XIAOMI";
+  constructor(marca) {
+    this.marca = marca;
+  }
+  toString() {
+    let mensaje = "[Marca=" + this.marca + "]";
+  }
+}
+
+class TelefonoMobil extends Dispositovo {
   CPU = "nombreCpu";
   RAM = "nombreRam";
   Almacenamiento = 20;
   Ancho = 40;
   Alto = 50;
   numCamaras = 2;
-  constructor(cpu, ram, alma, anc, alt, num) {
+  constructor(cpu, ram, alma, anc, alt, num, marca) {
+    super(marca);
     this.CPU = cpu;
     this.RAM = ram;
     this.Almacenamiento = alma;
@@ -13,11 +24,30 @@ class TelefonoMobil {
     this.Alto = alt;
     this.numCamaras = num;
   }
-  mostrar() {
-    let contenido = this.toString();
-    return contenido;
+  toString() {
+    let mensaje =
+      "[CPU=" +
+      this.CPU +
+      "\n, RAM=" +
+      this.RAM +
+      "\n, Almacenamiento=" +
+      this.Almacenamiento +
+      "\n, Ancho=" +
+      this.Ancho +
+      "\n, Alto=" +
+      this.Alto +
+      "\n, NumCamaras=" +
+      this.numCamaras +
+      "\n, Marca=" +
+      this.marca +
+      "]";
+    return mensaje;
   }
 }
 let miMobil = new TelefonoMobil("123CpU", "324Ram", 20, 12, 4, 76);
 
-console.log(miMobil.mostrar());
+console.log(miMobil.toString());
+
+for (elemento of Object.getOwnPropertyNames(miMobil)) {
+  console.log(elemento);
+}
