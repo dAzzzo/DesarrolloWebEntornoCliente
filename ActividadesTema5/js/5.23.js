51aad3478e23322d10243accf40e48f22352f04c
Daniel <dandcom0601@g.educaand.es>
2024-01-21 11:44:41 +0100

    solucion final del problema con git, cambios y subida de info

--- ActividadesTema5/js/5.23.js
+++ ActividadesTema5/js/5.23.js
@@ -3,27 +3,6 @@
 //El metodo a usar es verClub()
 
 
-class Club{
-    letructor(anioFundacion = 'No especificado', numSocios = 0, nombreEstadio = 'No especificado', ciudad = 'No especificado', palmares = []){
-        this.anioFundacion = anioFundacion;
-        this.numSocios = numSocios;
-        this.nombreEstadio = nombreEstadio;
-        this.ciudad = ciudad;
-        this.palmares = palmares;
-    }
-
-    verClub(){
-        console.log(`Año de fundacion: ${this.anioFundacion}`);
-        console.log(`Num de socios: ${this.numSocios}`);
-        console.log(`Nombre de estadio: ${this.nombreEstadio}`);
-        console.log(`Ciudad: ${this.ciudad}`);
-        console.log(`Palmares:`);
-        this.palmares.forEach(titulo => {
-            console.log(`- ${titulo.nombre} (${titulo.año})`);
-        });
-        console.log('-------------------------');
-    }
-
 class Club {
   constructor(
     anioFundacion = "No especificado",
@@ -50,31 +29,23 @@ class Club {
     });
     console.log("-------------------------");
   }
-
 }
 
-//Creamos los 6 clubs con los diferentes parametros
+// Creamos los 6 clubs con los diferentes parametros
 let club1 = new Club();
 let club2 = new Club(1962);
 let club3 = new Club(1974, 124);
 let club4 = new Club(1981, 462, "Bernabeu");
 let club5 = new Club(1692, 803, "Saint Martheo", "Ciudad Real");
 
-let club6 = new Club(1952, 7264, "Cruspian", "Cadiz", [{nombre: 'Torneo A', anio: 1956}, {nombre: 'Torneo B', anio: 1968}]);
-
-club6 = new Club(1952, 7264, "Cruspian", "Cadiz", [
+let club6 = new Club(1952, 7264, "Cruspian", "Cadiz", [
   { nombre: "Torneo A", anio: 1956 },
   { nombre: "Torneo B", anio: 1968 },
 ]);
 
-
 // Ahora tengo que modificar los datos
 club1.anioFundacion = 1920;
 club2.numSocios = 3000;
-
-club3.nombreEstadio = 'Caligula';
-club4.ciudad = 'Mostoles';
-club5.palmares.push({ nombre: 'Torneo C', año: 1985 });
 
 club3.nombreEstadio = "Caligula";
 club4.ciudad = "Mostoles";
@@ -88,7 +59,5 @@ club2.verClub();
 club3.verClub();
 club4.verClub();
 club5.verClub();
-
 club6.verClub();
 
-club6.verClub();
