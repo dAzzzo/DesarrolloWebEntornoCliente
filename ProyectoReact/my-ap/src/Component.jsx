9847ce68547d064684001bf82687c3cdf1f1f93f
Daniel <dandcom0601@g.educaand.es>
2024-03-03 23:45:24 +0100

    react

---
+++ ProyectoReact/my-ap/src/Component.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react"
+
+export default function Component() {
+
+    const [text, setText] = useState("");
+    const [updated, setUpdated] = useState("");
+
+
+    const textOnChange = (event) => {
+      setText(event.target.value)
+    }
+    
+    const buttonOnclick = () => {
+      setUpdated(text)
+    }
+    
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={textOnChange}/>
+      <button onClick={buttonOnclick}>Actualizar</button>
+      <p>Texto input: {text}</p>
+      <p>Texto actualizar: {updated}</p>
+    </div>
+  )
+}
